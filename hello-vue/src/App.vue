94641dd0d2557833fe76e20ffb0ada5592d1b5f7
qchen71 <qchen71@ford.com>
2023-03-27 14:42:05 +0800

    项目初始化

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <todo-list>
+    <todo-item v-for='item in list'
+               v-bind:key='item.title'
+               v-bind:title='item.title'
+               v-bind:del='item.del'
+               v-on:delete='handleDelete'>
+      <template v-slot:pre-icon='{value}'>
+        <span>前置图标:{{value}}</span>
+      </template>
+    </todo-item>
+  </todo-list>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    TodoItem
+  },
+  data () {
+    return {
+      message: 'hello world',
+      list: [
+        { title: '课程1', del: false },
+        { title: '课程2', del: false }
+      ]
+    }
+  },
+  methods: {
+    handleDelete (title) {
+      console.log('handle delete :', title)
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ hello-vue/src/components/TodoItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <slot name="pre-icon" v-bind:value="value"></slot>
+    <span v-if="del">{{ title }}</span>
+    <span v-else style="text-decoration: line-through;">{{ title }}</span>
+    <slot name="suf-icon">默认图标</slot>
+    <button v-show="del" v-on:click="handleClick">删除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    del: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      value: Math.random()
+    }
+  },
+  methods: {
+    handleClick () {
+      console.log('点击了删除按钮')
+      this.$emit('delete', this.title)
+    }
+  }
+}
+</script>
